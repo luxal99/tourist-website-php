93092002052f36226504fa98dff967169af1c267
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-18 10:39:26 +0200

    Ui home page

--- index.php
+++ index.php
@@ -7,34 +7,93 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Niš</title>
     <link rel="stylesheet" href="css/index.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-light ">
-            <a class="navbar-brand" href="#">Niš</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
-                </div>
+<div class="header">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#">Niš</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Features</a>
+                <a class="nav-item nav-link" href="#">Pricing</a>
+                <a class="nav-item nav-link disabled" href="#">Disabled</a>
             </div>
-        </nav>
-        <div class="text-center">
-            <h1>Dobrodošli u Niš</h1>
-            <p>Nadamo se da ćemo ispuniti sva Vaša očekivanja</p>
-            <button class="default-btn">Više informacija</button>
+        </div>
+    </nav>
+    <div class="text-center">
+        <h1>Dobrodošli u Niš</h1>
+        <p>Nadamo se da ćemo ispuniti sva Vaša očekivanja</p>
+        <button class="default-btn">Više informacija</button>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h1 style="padding-top: 2em">Niška tvrđava</h1>
+            <p>Niška tvrđava je gradsko utvrđenje u centru Niša na desnoj obali Nišave, koje ima kontinuitet od oko dva
+                milenijuma konstantnog postojanja o čemu svedoče mnogobrojni arheološki nalazi.</p>
+            <button class="default-btn">Više o tvrđavi</button>
+        </div>
+        <div class="col-sm">
+            <img src="assets/img/article-37.jpg" class="img-fluid ">
         </div>
     </div>
+    <div class="row" style="margin-top: 15em">
+        <div class="col-sm">
+            <img src="assets/img/119.jpg" class="img-fluid ">
+        </div>
+        <div class="col-sm text-right">
+            <h1 style="padding-top: 2em">Ćele kula</h1>
+            <p>Niška tvrđava je gradsko utvrđenje u centru Niša na desnoj obali Nišave, koje ima kontinuitet od oko dva
+                milenijuma konstantnog postojanja o čemu svedoče mnogobrojni arheološki nalazi.</p>
+            <button class="default-btn">Više o tvrđavi</button>
+        </div>
+    </div>
+    <div class="facts">
+        <h1>
+            Zanimljive činjenice
+        </h1>
+        <ul>
+            <li>Ime Niš potiče od Rimskog naziva <b>"Nais","Naissus"</b></li>
+            <li>Otkrića na praistorijskom lokalitetu<b> Bubanj i Velika humska čuka</b> govore da je tu postojala
+                naseobina u
+                <b>periodu 5800—5500. godine pre naše ere</b>. Najstariji sloj pripada neolitu ili starčevačkoj
+                kulturnoj
+                grupi.Ovi lokaliteti pripadaju Bubanj Salkuca Krivodol kompleksu.
+            </li>
+            <li><b>Naziv grada</b>, najverovatnije, potiče iz doba kada su njime vladali <b>Kelti</b>. Prema najčešće
+                navođenoj tezi o
+                etimološkom poreklu imena grada, Nišu su ime dali Kelti, Navisos sa značenjem Vilin grad.[4]
+            </li>
+            <li>Prema istoričaru Edvardu Gibonu ("Decline and Fall of the Roman Empire" Vol. 2, XXV), u Medijani 364.
+                godine carevi Valentinijan I i Valens podelili su Rimsko carstvo i potom vladali kao savladari
+            </li>
+            <li> Niš je bio jedno od sedišta turske vojne i civilne vlasti, a u najdužem periodu nalazio se u
+                Smederevskom pašaluku.
+            </li>
+            <li><b>U prvoj polovini 18. veka je nastalo Kazandžijsko sokače, danas jedini očuvani deo stare niške
+                    čaršije.</b>
+            </li>
+        </ul>
+    </div>
+</div>
 </body>
 
 </html>
