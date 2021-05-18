3dc2222e42cc313f08dea66a783d3c47be91fa9b
luxal99 <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-18 13:07:33 +0200

    Add CV page

--- index.php
+++ index.php
@@ -31,20 +31,20 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="icon" href="assets/img/nis-grb-veliki1.png">
 </head>
 
 <body>
 <div class="header">
     <nav class="navbar navbar-expand-lg navbar-light ">
-        <a class="navbar-brand" href="#">Niš</a>
+        <a class="navbar-brand" href="#"><img class="img-fluid" src="assets/img/nis-grb-veliki1.png" width="50px"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
+                <a class="nav-item nav-link" href="pages/cv.php">About me</a>
                 <a class="nav-item nav-link" href="#">Pricing</a>
                 <a class="nav-item nav-link disabled" href="#">Disabled</a>
             </div>
